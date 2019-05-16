33c202f01224f0a6a9d510f32dbb32127c62c0eb
Erisvaldo <erisvaldocarvalhosilva@gmail.com>
2019-05-16 10:37:51 -0300

    django-form concluído

--- formularios/core/forms.py
+++ formularios/core/forms.py
@@ -1,4 +1,17 @@
 from django import forms
 
+BIRTH_YEAR_CHOICES = ('1980', '1981', '1982', '1983', '1984')
+FAVORITE_COLORS_CHOICES = (
+    ('azul', 'Azul'),
+    ('verde', 'Verde'),
+    ('preto', 'Preto'),
+    )
+
 class NameForm(forms.Form):
     name = forms.CharField(label='Seu nome', max_length=100)
+    birth_year = forms.DateField(widget = forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+    favorite_colors = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=FAVORITE_COLORS_CHOICES,
+    )
--- formularios/core/views.py
+++ formularios/core/views.py
@@ -11,5 +11,16 @@ def form_manual(request):
     return render(request, 'core/index.html', data)
 
 def django_form(request):
-    form = NameForm()
-    return render(request, 'core/django-form.html', {'form':form})
+    form = NameForm(request.POST or None)
+    
+    name = ''
+    birth_year = ''
+    favorite_colors = ''
+
+    if request.method == 'POST':
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            birth_year = form.cleaned_data['birth_year']
+            favorite_colors = form.cleaned_data['favorite_colors']
+
+    return render(request, 'core/django-form.html', {'form':form, 'name':name, 'birth_year':birth_year,'favorite_colors':favorite_colors})
