c7d25f92bc897f8dcc59d547f9539dbd86ddf2b0
Erisvaldo <erisvaldocarvalhosilva@gmail.com>
2019-05-16 09:34:03 -0300

    Corrigido o problema ao acessar django-form

--- formularios/formularios/urls.py
+++ formularios/formularios/urls.py
@@ -4,6 +4,6 @@ from core.views import form_manual, django_form
 
 urlpatterns = [
     url(r'^$', form_manual, name = 'core_form_manual'),
-    url(r'^django-form/$', django_form,'core_from_django'),
+    url(r'^django-form/$', django_form, name = 'core_from_django'),
     url(r'^admin/', admin.site.urls),
 ]
